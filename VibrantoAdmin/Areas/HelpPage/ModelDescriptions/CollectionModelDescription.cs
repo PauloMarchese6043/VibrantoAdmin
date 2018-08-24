b20158b91598237f12f76311b1419de5394efd6e
Paulo Marchese <paulohenriquerm@hotmail.com>
2018-08-24 13:17:00 +1000

    Add project files.

---
+++ VibrantoAdmin/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace VibrantoAdmin.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ VibrantoAdmin/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VibrantoAdmin.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
